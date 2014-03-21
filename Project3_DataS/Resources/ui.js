69e81d76c8026e59c5ac652a342bc3342bda9287
b02202 <robertbrooks@Roberts-MacBook-Pro.local>
2014-03-21 16:20:55 -0400

    Final Submission

--- Project3_DataS/Resources/ui.js
+++ Project3_DataS/Resources/ui.js
@@ -1,9 +1,14 @@
+// Robert Brooks
+// AVF 1403
+// Project 3 Data Sync
+// ui.js
 
 
-
+var Cloud = require('ti.cloud');
 Ti.include('form.js');
-
-
+//var gallery = require('gallery');
+//Ti.include('gallery.js');
+var currentUser;
 var container = Ti.UI.createView ({
 	height: '100%',
 	width: '100%'
@@ -50,12 +55,206 @@ var userContainer = Ti.UI.createView ({
 	//backgroundImage: 'bg.png'
 	
 });
+var storyBtn = Ti.UI.createButton({
+	title: 'CREATE NEWS',
+	font: {fontSize: 25, fontFamily: "Arial"},
+	color: '#fff',
+	backgroundColor: '#b5b7b9',
+	borderColor: '#fff',
+	borderWidth: 1,
+	width: '50%',
+	height: 75,
+	bottom: 0,
+	right: 0,
+	enabled: false
+	
+});
+
+
+// *** Gallery ***
+var storyWin = Ti.UI.createWindow({
+	title: 'Your Stories',
+	backgroundColor: '#fff',
+});
+
+var galWin = Ti.UI.createWindow({
+	title: 'Photo Gallery',
+	backgroundColor: '#fff',
+});
+var titleField1 = Ti.UI.createTextField({
+	borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+  	color: '#336699',
+	top: 20,
+	left: 10,
+	right: 10, 
+	height: 40,
+	hintText: "Your Story Title."
+});
+
+
+
+var gallBtn = Ti.UI.createButton({
+	title: '+ from Photo Gallery',
+	font: {fontSize: 25, fontFamily: "Arial"},
+	color: '#fff',
+	backgroundColor: '#0099ff',
+	borderColor: '#fff',
+	borderWidth: 1,
+	width: '100%',
+	height: 60,
+	top: 20,
+	//right: '25%',
+	enabled: true
+	
+});
+
+var backButton = Ti.UI.createButton({
+	title: '<< BACK',
+	font: {fontSize: 25, fontFamily: "Arial"},
+	color: '#fff',
+	backgroundColor: '#0099ff',
+	borderColor: '#fff',
+	borderWidth: 1,
+	width: '100%',
+	height: 60,
+	bottom: 0,
+	//right: '25%',
+	enabled: true
+	
+});
+backButton.addEventListener('click', function(){
+				galWin.close();
+				storyWin.close();
+			});
+galWin.add(backButton);
+
+gallBtn.addEventListener('click', function(e) {
+	Ti.Media.openPhotoGallery({
+		allowEditing: true,
+		success: function(e){
+			var picFormView = Ti.UI.createScrollView({
+				layout: "vertical",
+				backgroundColor: "#000",
+				width: '100%',
+				top: '50%',
+				bottom: 60
+
+			});
+			var imgView = Ti.UI.createImageView({
+				image: e.media,
+				width: '50%',
+				//height: '50%',
+				top: 85,
+				bottom: '52%',
+				left: '25%'
+			});
+
+			
+			var sendBtn = Ti.UI.createButton({
+				title: "Upload to cloud",
+				color: "#fff",
+				font: {fontSize: 25, fontFamily: "Helvetica"},
+				textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+			    backgroundColor: "#0099ff",
+			    borderRadius: 20,
+			    borderColor: "#fff",
+			    height: 50,
+			    top: 20,
+			    left: 40,
+			    right: 40,
+			    enabled: true
+ 
+			});
+			picFormView.add(titleField1);
+			//picFormView.add(fullTxtField);
+			picFormView.add(sendBtn);
+			galWin.add(picFormView);
+			galWin.add(imgView);
+			galWin.open();
+			
+			sendBtn.addEventListener('click', function(){
+				
+				var img = imgView.toImage();
+				dataFile = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, titleField1.value +'.png');
+				if (dataFile.exists()){
+					dataFile.deleteFile();
+				}
+					
+					//dataFile.createFile();
+					dataFile.write(img);
+					var fileContent = dataFile.read();
+					
+					// push to ACS
+					Cloud.Photos.create({
+						photo: fileContent, //Titanium.Filesystem.getFile('photo.jpg')
+						user_id : currentUser
+						}, function (e) {
+							if (e.success) {
+							var photo = e.photos[0];
+						        alert('Success:\n' +
+						            'id: ' + photo.id + '\n' +
+						            'filename: ' + photo.filename + '\n' +
+						            'size: ' + photo.size + '\n' +
+						            'updated_at: ' + photo.updated_at + '\n' +
+						            'userid:' + currentUser);
+					
+						} else {
+						        alert('Error:\n' +
+						            ((e.error && e.message) || JSON.stringify(e)));
+						    }
+						});
+					
+					//emailDialog.addAttachment(fileContent);
+					//emailDialog.subject = titleField.value;
+					//emailDialog.messageBody = fullTxtField.value + '\n' + 'Location = ' + coordText;
+					//emailDialog.open();
+				
+				//dataFile.createFile();
+				dataFile.write(img);
+				var fileContent = dataFile.read();
+				//emailDialog.addAttachment(fileContent);
+				//emailDialog.subject = titleField.value;
+				//emailDialog.messageBody = fullTxtField.value + '\n' + 'Location = ' + coordText;
+				//emailDialog.open();
+			});
+			
+			/*emailDialog.addEventListener('complete', function(e){
+				alert('Your Email Has Been Sent.');
+				if(dataFile.exists()){
+					dataFile.deleteFile();
+				}
+			
+			});*/
+			
+			
+		},
+		error: function(error) {
+			if (error.code == Ti.Media.NO_CAMERA) {
+				alert("please run this on a device");
+			} else {
+				alert("other error: " + error.code);
+			}
+		},
+		cancel: function(e){
+			alert("Photo gallery was canceled");
+		}
+		
+	});
+	titleField1.value = "";
+});
+
+storyBtn.addEventListener('click', function(){
+	storyWin.open();
+});
+
 
 // Button Events ***
 submitButton.addEventListener('click', function(){
 	if (emailTxt.value == '' || passTxt.value == ''){
 		alert('Please enter a username and password.');
 	} else{
+		storyBtn.enabled = true;
+		storyBtn.bacgroundColor = '#0099ff'; 
 		(function() { 
 	// load the Cloud Module
 	var Cloud = require('ti.cloud');
@@ -78,6 +277,7 @@ submitButton.addEventListener('click', function(){
 					welcomeLabel.visible = true;
 					name = user.first_name;
 					welcomeLabel.text = 'Welcome ' + name + '!'; //+ '\n' + 'Please Choose Top News or Favorites Below.';
+					currentUser = user.id;
 			} else {
 				alert((e.error && e.message) || JSON.stringify(e));
 			}
@@ -86,17 +286,10 @@ submitButton.addEventListener('click', function(){
 	loginUser();
 	// now your app is ready to access ACS network and data services
 })();
-
-
-		
-		//name = emailTxt.value;
-		
-		
-		// hide keyboard
+		//hide keyboard
 		emailTxt.blur();
 		passTxt.blur();
 	}
-	
 });
 
 submitButton2.addEventListener('click', function(){
@@ -129,17 +322,16 @@ submitButton2.addEventListener('click', function(){
 				welcomeLabel2.visible = true;
 				name = user.first_name;
 				welcomeLabel2.text = 'Welcome ' + name  + '!'; // + '\n' + 'Please Choose Top News or Favorites Below.';
+				currentUser = user.id;
 			} else {
 				alert((e.error && e.message) || JSON.stringify(e));
 			}
 		});
-	}; // loginUser ends
+	}; // userCreate ends
 	userCreate();
 	// now your app is ready to access ACS network and data services
 })();
 
-		
-		
 		// hide keyboard
 		emailTxt.blur();
 		passTxt.blur();
@@ -149,12 +341,13 @@ submitButton2.addEventListener('click', function(){
 
 createLogin.addEventListener('click', function(){
 	userForm.add(userContainer);
+	userForm.add(storyBtn);
 	userForm.open();
-	
-	
+	storyBtn.enabled=true;
+	storyBtn.backgroundColor = '#0099ff';	
 });
 
-//var user = require('user');
+
 
 userContainer.add(formView2);
 userContainer.add(welcomeLabel2);
@@ -163,3 +356,9 @@ container.add(headline);
 container.add(welcomeLabel);
 container.add(formView);
 win1.add(container);
+win1.add(storyBtn);
+
+// *** gallery add ***
+storyWin.add(gallBtn);
+//storyWin.add(picBtn);
+storyWin.add(backButton);
